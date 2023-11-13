f29e6089251e7a0891e92c274fe0f2250205ad7c
toru-a <148777061+toru-a@users.noreply.github.com>
2023-11-13 12:46:15 +0900

    Create "week5"app.js

---
+++ app.js
@@ -0,0 +1,65 @@
+
+//D112:工場の生産力
+
+reader.on('close', () => {
+  console.log(lines[0]*lines[1]);
+});
+
+
+//D190:センチをミリに
+
+reader.on('close', () => {
+  const mm = 10
+  console.log(lines[0] * mm);
+});
+
+
+//D123:自動でチャージ
+
+reader.on('close', () => {
+    const charge = 10000
+    const left = parseInt(lines)
+    if(left < charge){
+        console.log( left + charge );
+    }else{
+  console.log(lines[0]);
+
+
+//C084:【キャンペーン問題】枠で囲む
+
+reader.on('close', () => {
+ let len = lines[0].length
+ let TBlen = len+2
+ let symbol = "+"
+ console.log(symbol.repeat(TBlen));
+ console.log(symbol+lines[0]+symbol);
+ console.log(symbol.repeat(TBlen));
+});
+
+
+
+//FIZZBAZZ問題
+
+let index = 0;
+let number = 1;
+
+while( number <= 100 ){
+ 
+ let ListB = ["FIZZBAZZ","BAZZ","FIZZ",number];
+ let ListA = ["15","5","3",number];
+ const ALen= ListA.length;
+
+ while( index < ALen ){
+ 
+  if( number % ListA[index] == 0 ){
+     console.log(ListB[index]);
+     index = ALen;
+    }else{
+     index++;
+    };    
+  };
+   index = 0;
+   number++;
+
+};
+ 
